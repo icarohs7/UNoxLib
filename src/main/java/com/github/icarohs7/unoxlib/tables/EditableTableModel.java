a6d094fa7cac9023ad1c5f4b828da840d5f4674b
Icaro D Temponi <icarohs7@gmail.com>
2018-05-02 10:42:29 -0300

    V0.1.1  -->Acentuação removida para evitar problemas de incompatibilidade
    Signed-off-by: Icaro D Temponi <icarohs7@gmail.com>

--- src/main/java/com/github/icarohs7/unoxlib/tables/EditableTableModel.java
+++ src/main/java/com/github/icarohs7/unoxlib/tables/EditableTableModel.java
@@ -1,10 +1,10 @@
 package com.github.icarohs7.unoxlib.tables;
 
 /**
- * Implementação do modelo de tabela que permite a edição da mesma após a
- * instanciação<br>
+ * Implementacao do modelo de tabela que permite a edicao da mesma apos a
+ * instanciacao<br>
  * <br>
- * Feito por codejava.net, Código e documentação em<br>
+ * Feito por codejava.net, Codigo e documentacao em<br>
  * <a href="http://www.codejava.net/java-se/swing/editable-jtable-example">
  * http://www.codejava.net/java-se/swing/editable-jtable-example
  * </a>
@@ -12,7 +12,7 @@ package com.github.icarohs7.unoxlib.tables;
 public class EditableTableModel extends javax.swing.table.AbstractTableModel {
 	
 	/**
-	 * Títulos das colunas
+	 * Titulos das colunas
 	 */
 	private String[] columnTitles;
 	
@@ -38,9 +38,9 @@ public class EditableTableModel extends javax.swing.table.AbstractTableModel {
 	}
 	
 	/**
-	 * Retorna o número de linhas contidas na tabela
+	 * Retorna o numero de linhas contidas na tabela
 	 *
-	 * @return O número de linhas da tabela
+	 * @return O numero de linhas da tabela
 	 */
 	@Override
 	public int getRowCount() {
@@ -48,9 +48,9 @@ public class EditableTableModel extends javax.swing.table.AbstractTableModel {
 	}
 	
 	/**
-	 * Retorna o número de colunas contidas na tabela
+	 * Retorna o numero de colunas contidas na tabela
 	 *
-	 * @return O número de colunas da tabela
+	 * @return O numero de colunas da tabela
 	 */
 	@Override
 	public int getColumnCount() {
@@ -58,12 +58,12 @@ public class EditableTableModel extends javax.swing.table.AbstractTableModel {
 	}
 	
 	/**
-	 * Retorna o valor contido na célula selecionada
+	 * Retorna o valor contido na celula selecionada
 	 *
-	 * @param row    Indice da linha da célula a ser selecionada, de 0 a n
-	 * @param column Indice da coluna da célula a ser selecionada, de 0 a n
+	 * @param row    Indice da linha da celula a ser selecionada, de 0 a n
+	 * @param column Indice da coluna da celula a ser selecionada, de 0 a n
 	 *
-	 * @return O conteúdo da célula selecionada
+	 * @return O conteudo da celula selecionada
 	 */
 	@Override
 	public Object getValueAt( int row, int column ) {
@@ -83,12 +83,12 @@ public class EditableTableModel extends javax.swing.table.AbstractTableModel {
 	}
 	
 	/**
-	 * Retorna se a célula indicada é ou não editável
+	 * Retorna se a celula indicada e ou nao editavel
 	 *
-	 * @param row    Indice da linha da célula a ser selecionada, de 0 a n
-	 * @param column Indice da coluna da célula a ser selecionada, de 0 a n
+	 * @param row    Indice da linha da celula a ser selecionada, de 0 a n
+	 * @param column Indice da coluna da celula a ser selecionada, de 0 a n
 	 *
-	 * @return Se a célula é ou não editável
+	 * @return Se a celula e ou nao editavel
 	 */
 	@Override
 	public boolean isCellEditable( int row, int column ) {
@@ -96,11 +96,11 @@ public class EditableTableModel extends javax.swing.table.AbstractTableModel {
 	}
 	
 	/**
-	 * Edita uma célula da tabela
+	 * Edita uma celula da tabela
 	 *
-	 * @param value  O conteúdo que se deseja inserir na célula
-	 * @param row    Indice da linha onde a célula desejada se encontra, de 0 a n
-	 * @param column Indice da coluna onde a célula desejada se encontra, de 0 a n
+	 * @param value  O conteudo que se deseja inserir na celula
+	 * @param row    Indice da linha onde a celula desejada se encontra, de 0 a n
+	 * @param column Indice da coluna onde a celula desejada se encontra, de 0 a n
 	 */
 	@Override
 	public void setValueAt( Object value, int row, int column ) {
--- src/main/java/com/github/icarohs7/unoxlib/tables/ScrollTable.java
+++ src/main/java/com/github/icarohs7/unoxlib/tables/ScrollTable.java
@@ -1,42 +1,40 @@
 package com.github.icarohs7.unoxlib.tables;
 
 
-import com.sun.istack.internal.NotNull;
-
 import java.awt.Dimension;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 /**
- * Sobrescrita da JTable padrão para fazer o tamanho do JScroll se adaptar automaticamente à tabela
+ * Sobrescrita da JTable padrao para fazer o tamanho do JScroll se adaptar automaticamente a tabela
  *
  * @author <a href="https://github.com/icarohs7">Icaro D Temponi</a>
  */
 public class ScrollTable extends JTable {
 	/**
-	 * Instanciar uma tabela mutável com scroll
+	 * Instanciar uma tabela mutavel com scroll
 	 *
 	 * @param data        Os dados a serem inseridos na tabela
 	 * @param columnNames Os nomes das colunas da tabela
 	 *
 	 * @return the scroll table
 	 */
-	public static ScrollTable ofMutableCells( @NotNull Object[][] data, @NotNull String[] columnNames ) {
+	public static ScrollTable ofMutableCells( Object[][] data, String[] columnNames ) {
 		ScrollTable table = new ScrollTable( data, columnNames );
 		table.setModel( new EditableTableModel( data, columnNames ) );
 		return table;
 	}
 	
 	/**
-	 * Instanciar uma tabela imutável com scroll
+	 * Instanciar uma tabela imutavel com scroll
 	 *
 	 * @param data        Os dados a serem inseridos na tabela
 	 * @param columnNames Os nomes das colunas da tabela
 	 */
-	public ScrollTable( @NotNull Object[][] data, @NotNull String[] columnNames ) {
+	public ScrollTable( Object[][] data, String[] columnNames ) {
 		super( data, columnNames );
-		/* Desativar a reordenação das colunas */
+		/* Desativar a reordenacao das colunas */
 		tableHeader.setReorderingAllowed( false );
 		/* Desativar o redimensionamento das colunas */
 		tableHeader.setResizingAllowed( false );
@@ -52,7 +50,7 @@ public class ScrollTable extends JTable {
 	}
 	
 	/**
-	 * Desativar a seleção de células
+	 * Desativar a selecao de celulas
 	 *
 	 * @param i  the
 	 * @param i1 the 1
@@ -65,7 +63,7 @@ public class ScrollTable extends JTable {
 	}
 	
 	/**
-	 * Desativar a edição de células
+	 * Desativar a edicao de celulas
 	 *
 	 * @param i  the
 	 * @param i1 the 1
